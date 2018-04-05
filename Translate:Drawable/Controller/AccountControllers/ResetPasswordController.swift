7c613b28649570103809f443d97e7614fc53f8dd
Jacques Castonguay <jgc009@gmail.com>
2018-04-05 16:51:01 -0500

    adds password reset

--- Translate:Drawable/Controller/AccountControllers/ResetPasswordController.swift
+++ Translate:Drawable/Controller/AccountControllers/ResetPasswordController.swift
@@ -7,9 +7,48 @@
 //
 
 import UIKit
+import Firebase
 
 class ResetPasswordController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    
+    @IBAction func resetPassword(sender: UIButton) {
+        //Validate input
+        guard let emailAdress = emailTextField.text, emailAdress != "" else {
+            let alertController = UIAlertController(title: "Input Error", message: "please provide an email for password reset", preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(okayAction)
+            present(alertController, animated: true, completion: nil)
+            
+            return
+        }
+        
+        //Send the password reset to the email
+        Auth.auth().sendPasswordReset(withEmail: emailAdress, completion: {(error) in
+            let title = (error == nil) ? "Password reset Follow-up" : "Password Reset Error"
+            let message = (error == nil) ? "We have just sent you a password reset email. Please check your inbox and follow the instructions to reset your password." : error?.localizedDescription
+            
+            let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: {
+                (action) in
+                if error == nil {
+                    //Dismiss keyboard
+                    self.view.endEditing(true)
+                    
+                    //Return to the login screen
+                    if let navController = self.navigationController {
+                        navController.popViewController(animated: true)
+                    }
+                }
+            })
+            alertController.addAction(okayAction)
+            
+            self.present(alertController, animated: true, completion: nil)
+        })
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
