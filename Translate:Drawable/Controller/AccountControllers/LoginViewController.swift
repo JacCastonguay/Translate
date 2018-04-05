99905a22572e130c030ee9521684c8aa7e5029d7
Jacques Castonguay <jgc009@gmail.com>
2018-04-04 22:31:36 -0500

    login works

--- Translate:Drawable/Controller/AccountControllers/LoginViewController.swift
+++ Translate:Drawable/Controller/AccountControllers/LoginViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import Firebase
+
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +26,57 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func login(sender: UIButton) {
+        //validation again
+        guard let emailAddress = emailTextField.text, emailAddress != "",
+            let password = passwordTextField.text, password != "" else {
+                let alertController = UIAlertController(title: "Login Error", message: "Neither field can be blank", preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                present(alertController, animated: true, completion: nil)
+                
+                return
+        }
+        
+        //Perform login by calling Firebase APIs
+        Auth.auth().signIn(withEmail: emailAddress, password: password, completion: {
+            (user, error) in
+            if let error = error {
+                let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                self.present(alertController, animated: true, completion: nil)
+                
+                return
+            }
+            
+            //Email verification
+            guard let currentUser = user, currentUser.isEmailVerified else {
+                let alertController = UIAlertController(title: "Login Error", message: "You haven't confirmed your email yet. Please click the verification link in the email we sent. If you need us to resend it tap Resend Email", preferredStyle: .alert)
+                let okayAction = UIAlertAction(title: "Resend Email", style: .default, handler: {
+                    (action) in
+                    user?.sendEmailVerification(completion: nil)
+                })
+                let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+                alertController.addAction(okayAction)
+                alertController.addAction(cancelAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+                
+                return
+            }
+            
+            //Dismiss keyboard
+            self.view.endEditing(true)
+            
+            //Present the main view
+            if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+                UIApplication.shared.keyWindow?.rootViewController = viewController
+                self.dismiss(animated: true, completion: nil)
+            }
+            
+        })
+    }
 
     /*
     // MARK: - Navigation
